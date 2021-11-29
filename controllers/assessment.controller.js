23deaeb1060a670125f782adcf6b67562ed1a300
Kevin Mezu <kevinmezu@Kevins-Mac-mini.fios-router.home>
2021-11-28 23:19:19 -0500

    getCurrentAssessment controller

--- controllers/assessment.controller.js
+++ controllers/assessment.controller.js
@@ -2,12 +2,12 @@ const Assessment = require('../models/assessment.model');
 
 
 exports.logAssesment = async (req,res) => {
-    if (!req.body.id || !req.body.userId) {
+    if (!req.body.id) {
         res.status(400).send("Error: Must have relevant id to traitify quiz");
         return;
     }
     const allTests = await Assessment.findAll({where : {
-        userId: req.body.userId
+        userId: req.user.id
     }});
     
     unFinishedTests = await allTests.flatMap((item, index)=>{
@@ -23,7 +23,7 @@ exports.logAssesment = async (req,res) => {
     } else {
         const assessment = {
             uuid : req.body.id,
-            userId : req.body.userId
+            userId : req.user.id
         }
         Assessment.create(assessment).then((data)=>{
             res.status(200).json({
@@ -38,7 +38,23 @@ exports.logAssesment = async (req,res) => {
     })
     }
 }
-
+exports.getCurrentAssessment = async (req,res) => {
+    try {
+        if (req.user) {
+            const currentAssessment = await Assessment.findOne({where:{
+                isComplete : false,
+                userId: req.user.id
+            }});
+            if (currentAssessment) {
+                res.status(200).send(currentAssessment);
+            } else {
+                res.status(400).send({message: "Error: No current assessment or all assessments completed, create a new assessment."})
+            }
+        }
+    } catch (err) {
+        res.status(500).send({message: err.message})
+    }
+}
 exports.completeAssessment = async (req,res) => {
     const assessment = await Assessment.findByPk(req.params.id);
 
--- routes/assessment.routes.js
+++ routes/assessment.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router  = express.Router();
-const assessmentController = require('../controllers/assessment.controller')
+const assessmentController = require('../controllers/assessment.controller');
+const { isAuth } = require('../models/authtoken.model');
 
-router.post('/', assessmentController.logAssesment);
-router.put('/:id', assessmentController.completeAssessment)
+router.post('/', isAuth, assessmentController.logAssesment);
+router.get('/current-assessment', isAuth, assessmentController.getCurrentAssessment)
+router.put('/:id', isAuth, assessmentController.completeAssessment)
 
 module.exports = router;
